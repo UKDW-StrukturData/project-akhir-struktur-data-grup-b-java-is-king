c45b6e950d5f25601d79e8f31247f1082bbf3450
71241138 <71241138@students.ukdw.ac.id>
2025-12-17 11:36:44 +0700

    f
    f

--- pages/home.py
+++ pages/home.py
@@ -11,7 +11,7 @@ from io import BytesIO
 import json
 import random
 import numpy as np
-import openai
+import openai as genai
 
 # Tambahkan path untuk import utils
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
